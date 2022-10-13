976b673c5dd1ee5faf44a7427653535829ca788f
Tisha Madame <112636486+TishaRepo@users.noreply.github.com>
2022-10-14 01:27:44 +0530

    Add files via upload
    Changed UI , along with join bugs

--- index.js
+++ index.js
@@ -1,191 +1,237 @@
-const { create } = require("domain");
+const {
+    create
+} = require("domain");
 const http = require("http");
-const app= require("express")();
-app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
-app.listen(9091,()=>console.log("Listening on http port 9091"))
-const websocketServer =require("websocket").server
+const app = require("express")();
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
+app.listen(9091, () => console.log("Listening on http port 9091"))
+const websocketServer = require("websocket").server
 const httpServer = http.createServer();
-httpServer.listen(9090,() => console.log("Listening.. on 9090"))
+httpServer.listen(9090, () => console.log("Listening.. on 9090"))
 
-const clientdict={};
+const clientdict = {};
 const games = {};
-var currentPattern = {patternNo:0,pattern:[]};
-const wsServer = new websocketServer({"httpServer":httpServer})
-wsServer.on("request", request =>{
-    const connection = request.accept(null,request.origin);
-    connection.on("open",() => console.log("opened!"))
-    connection.on("closed",() => console.log("closed!"))
-    connection.on("message",message => { 
-        const receivedfromclient =JSON.parse(message.utf8Data)
+var currentPattern = {
+    patternNo: 0,
+    pattern: []
+};
+
+
+const wsServer = new websocketServer({
+    "httpServer": httpServer
+})
+wsServer.on("request", request => {
+    const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("opened!"))
+    connection.on("closed", () => console.log("closed!"))
+    connection.on("message", message => {
+        const receivedfromclient = JSON.parse(message.utf8Data)
         console.log(receivedfromclient)
 
 
-           //player has sent create request
+        //player has sent create request
 
-        if(receivedfromclient.method==="create"){
-            const cid=receivedfromclient.cid;
-           
-            const gameid=genid();
-            games[gameid]={
-                "id":gameid,
-                "balls":20,
-                "clientdict":[ ]
+        if (receivedfromclient.method === "create") {
+            const cid = receivedfromclient.cid;
+
+            const gameid = genid();
+            games[gameid] = {
+                "id": gameid,
+                "balls": 20,
+                "clientdict": []
             }
 
-            const send ={
-                "method":"create",
-                "game":games[gameid]
+            const send = {
+                "method": "create",
+                "game": games[gameid]
             }
 
-            const con =clientdict[cid].connection;
-            con.send(JSON.stringify(send)); 
+            const con = clientdict[cid].connection;
+            con.send(JSON.stringify(send));
         }
 
 
-           //player has sent start request
-        if(receivedfromclient.method === "start"){
+        //player has sent start request
+        if (receivedfromclient.method === "start") {
+            const gid = receivedfromclient.gameIdGlobal;
+            const game = games[gid];
+            if (game.clientdict.length < 2) {
+                const send = {
+                    "method": "minPlayers"
+                }
+
+                game.clientdict.forEach(c => {
+                    clientdict[c.cid].connection.send(JSON.stringify(send))
+                })
+                return;
+            }
             const pattern = GetPattern();
-            currentPattern.pattern=pattern;
-            updateClientPattern(pattern,-1);
+            currentPattern.pattern = pattern;
+            updateClientPattern(pattern, -1);
+            SetTimer();
         }
 
 
 
-            //player has sent join request
-        if(receivedfromclient.method === "join")
-        {
+        //player has sent join request
+        if (receivedfromclient.method === "join") {
+
+            var name = receivedfromclient.playerName;
             const cid = receivedfromclient.cid;
             const gameid = receivedfromclient.gameid;
             const game = games[gameid];
-            if (game.clientdict.length >= 2) 
-            {
-               
+
+            if (game.clientdict.length > 2) {
+
+
                 return;
             }
-            const color =  {"0": "Red", "1": "Green"}[game.clientdict.length]
+
+            const color = {
+                "0": "Red",
+                "1": "Green"
+            } [game.clientdict.length]
             game.clientdict.push({
                 "cid": cid,
-                "color": color
+                "color": color,
+                "name": name
             })
-           
-            
+
+
 
             const send = {
                 "method": "join",
                 "game": game
             }
-           
+
             game.clientdict.forEach(c => {
                 clientdict[c.cid].connection.send(JSON.stringify(send))
             })
         }
 
 
-          // we'll start playing once we click a cell i.e a button
-        if(receivedfromclient.method === "play"){
-            const gameid = receivedfromclient.gameid;
-            const game = games[gameid];
-            const cid = receivedfromclient.cid;
-            const ballId = receivedfromclient.ballId;
-            const color = receivedfromclient.color;
-            const pattern=[7,2,9];
-            if(game.clientdict.length < 2)
-            {
-                const send = {
-                    "method": "eplayers",
-                     "game": game
+        if (receivedfromclient.method === "onWallBreak") {
+            let cid = receivedfromclient.cid;
+            let buttonNumber = receivedfromclient.buttonNumber;
+            let TempCurrentPattern = currentPattern.pattern;
+            let s = clientdict[cid].score
+            s = s + 1;
+            clientdict[cid].score = s;
+            TempCurrentPattern.splice(TempCurrentPattern.indexOf(buttonNumber), 1);
+            if (TempCurrentPattern.length <= 0) {
+
+
+                currentPattern.patternNo = currentPattern.patternNo + 1;
+                TempCurrentPattern = GetPattern();
+                currentPattern.pattern = TempCurrentPattern;
+                if (TempCurrentPattern.includes(buttonNumber)) {
+                    buttonNumber = -1;
                 }
-               
-                game.clientdict.forEach(c => {
-                    clientdict[c.cid].connection.send(JSON.stringify(send))
-                })
-                
             }
-
-            let state = games[gameid].state;
-            if (!state)
-                state = {}
-            
-            state[ballId] = color;
-            games[gameid].state = state;
+            updateClientPattern(TempCurrentPattern, buttonNumber);
         }
 
+    })
 
-
-
-        if(receivedfromclient.method === "onWallBreak")
-        {   
-            
-            let buttonNumber =receivedfromclient.buttonNumber;
-            let TempCurrentPattern=currentPattern.pattern;
-            TempCurrentPattern.splice(TempCurrentPattern.indexOf(buttonNumber),1);
-            if(TempCurrentPattern.length<=0){
-                
-                
-                currentPattern.patternNo=currentPattern.patternNo+1;
-                TempCurrentPattern=GetPattern();
-                currentPattern.pattern=TempCurrentPattern;
-                if(TempCurrentPattern.includes(buttonNumber))
-                {buttonNumber = -1;}
-            }
-            updateClientPattern(TempCurrentPattern,buttonNumber);
-        }
-         
-    }) 
-    
     const cid = genid();
 
-    clientdict[cid]={
-       "connection":connection
+    clientdict[cid] = {
+        "connection": connection,
+        "score": 0,
+
     }
 
-    const response ={
-       "method":"connect",
-       "cid":cid
+    const response = {
+        "method": "connect",
+        "cid": cid
     }
     connection.send(JSON.stringify(response))
 
 })
 
-function updateClientPattern(p_arrPattern,removeId){
-  
+function updateClientPattern(p_arrPattern, removeId) {
+
+    var clientInfo = GetScore();
+
     for (const g of Object.keys(games)) {
-        
+
         const game = games[g]
         var sendtoclient = {
-        "method": "patternupdate",
-        "game": game,
-        "pattern":p_arrPattern,
-        "removeId":removeId,
-        "isReverse":true
-      }
+            "method": "patternupdate",
+            "game": game,
+            "pattern": p_arrPattern,
+            "removeId": removeId,
+            "isReverse": true,
+            "ScoreInfo": clientInfo
 
-     game.clientdict.forEach(c=> {
-        clientdict[c.cid].connection.send(JSON.stringify(sendtoclient));
-        sendtoclient.isReverse=!sendtoclient.isReverse;
-      })
-      }
-}
 
-function GetPattern()
-{  if(currentPattern.patternNo<0 || currentPattern.patternNo>4 )
-    {
-      currentPattern.patternNo=0;
-      
+        }
+
+        game.clientdict.forEach(c => {
+            clientdict[c.cid].connection.send(JSON.stringify(sendtoclient));
+            sendtoclient.isReverse = !sendtoclient.isReverse;
+        })
     }
-   let PatternObject = { 
-    0:[7,4,9] ,
-    1:[4,7,3] ,
-    2:[2,15,18] ,
-    3:[17,14,19] ,
-    4:[12,8,6] 
-  }  
-  return PatternObject[currentPattern.patternNo];
+}
+
+function GetScore() {
+    var arrClientInfo = [];
+    for (const key in clientdict) {
+        let objClient = {}
+
+        objClient["clientId"] = key;
+        objClient["score"] = clientdict[key].score;
+
+        arrClientInfo.push(objClient);
+    }
+
+
+    return arrClientInfo;
+}
+
+function GetPattern() {
+    if (currentPattern.patternNo < 0 || currentPattern.patternNo > 4) {
+        currentPattern.patternNo = 0;
+
+    }
+    let PatternObject = {
+        0: [7, 4, 9],
+        1: [4, 7, 3],
+        2: [2, 15, 18],
+        3: [17, 14, 19],
+        4: [12, 8, 6]
+    }
+    return PatternObject[currentPattern.patternNo];
+}
+
+function SetTimer() {
+    var second = 20;
+    var interval = setInterval(function () {
+        if (second == 0) {
+            clearInterval(interval)
+        }
+
+
+        for (const g of Object.keys(games)) {
+
+            const game = games[g]
+            const sendtoclient = {
+                "method":"updateTimer",
+                "second": second
+            }
+
+            game.clientdict.forEach(c => {
+                clientdict[c.cid].connection.send(JSON.stringify(sendtoclient));
+                sendtoclient.isReverse = !sendtoclient.isReverse;
+            })
+        }
+
+        second = second - 1;
+
+    }, 1000);
 }
 
 
-function S4() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
-}
- 
-const genid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substring(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+
+
+const genid = () => Math.floor(Math.random() * 900000 + 100000);
